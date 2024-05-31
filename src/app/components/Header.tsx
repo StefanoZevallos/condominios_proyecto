185656c251c1bfd9899cabc1eb4e455ed06ca3f8
Stefano Zevallos <szevallosnl@gmail.com>
2024-05-31 17:10:06 -0500

    logo mas pequeño

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,18 +7,18 @@ const Header = () => {
   return (
     <>
       <header className='bg-red-600 h-20 flex  items-center  w-full'>
-      <div className=' font-bold cursor-pointer text-white'>
+      <div className='text-md font-bold cursor-pointer text-white'>
          <Link href={"/"}>
           <p className='ml-4'>Negocios </p>
           <p className='ml-4'>Condominios </p>
          </Link> 
         </div>
-        <div className='ml-4 w-[35px]'>
+        <div className='ml-2 w-[32px]'>
       <Image className='cursor-pointer'
         src="https://res.cloudinary.com/dqpijrvsq/image/upload/v1717192579/condominios/nrx5hqepfvoqeh1gazas.png"
         alt="Descripción de la imagen"
-        width={80}
-        height={80}
+        width={1000}
+        height={1000}
       />
     </div>
 
