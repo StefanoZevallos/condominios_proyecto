662c461354a6d5198152816ca82863bf4f7f9fe7
Stefano Zevallos <szevallsnl@gmail.com>
2024-07-27 18:06:23 -0500

    footer retirado

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="bg-red-600 w-full flex flex-col  py-5 h-4/5">
                 <div className='flex flex-col justify-center align-center text-center'>
                     <p className='text-xs my-2 text-white'> Hecho con ❤️. Gracias por tu visita!   </p>
-                    <p className='text-xs my-2 text-white'> Envíanos una sugerencia: +51 926 802 518</p>
+                    <p className='text-xs my-2 text-white'> Contacto: +51 926 802 518</p>
                     {/* <p className='text-xs my-2 text-white'> Todos los derechos reservados  </p> */}
                 </div>
             </div>
